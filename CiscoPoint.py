65c8ff1863237c0bc63649454aaef7672eaaa125
Caiovlobof <caiovlobof@gmail.com>
2023-05-09 16:04:26 -0300

    Checkpoint

--- CiscoPoint.py
+++ CiscoPoint.py
@@ -1,3 +1,9 @@
-qualquer_coisa = input("Me conta algo..")
-print("Hm.. ",qualquer_coisa,"De verdade?")
- 
+a = float(input("Enter a number: "))
+b = float(input("Enter the second number: "))
+
+print("Soma result is: " + str(a + b))
+print("Subt result is: " + str(a - b))
+print("Mult result is: " + str(a * b))
+print("Divi result is: " + str(a / b))
+
+print("\nIsso é tudo, pessoal!")
