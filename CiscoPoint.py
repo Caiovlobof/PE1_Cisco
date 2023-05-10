a737008ecaa7bf22453fab2ce910921ef4bd17d0
Caiovlobof <caiovlobof@gmail.com>
2023-05-09 22:23:48 -0300

    checkpoint

--- CiscoPoint.py
+++ CiscoPoint.py
@@ -1,9 +1,2 @@
-a = float(input("Enter a number: "))
-b = float(input("Enter the second number: "))
-
-print("Soma result is: " + str(a + b))
-print("Subt result is: " + str(a - b))
-print("Mult result is: " + str(a * b))
-print("Divi result is: " + str(a / b))
-
-print("\nIsso é tudo, pessoal!")
+n = int(input("Enter number:"))
+print(n >= 100)
