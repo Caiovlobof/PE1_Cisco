a75a5edb7a98bb7aebd730e0b117c7307e13d370
Caiovlobof <caiovlobof@gmail.com>
2023-05-11 17:34:49 -0300

    checkpoint

--- CiscoPoint.py
+++ CiscoPoint.py
@@ -2,8 +2,11 @@ income = float(input("Digite aqui seu imposto: "))
 
 if income < 85528:
     tax = income * 0.18 - 556.02
-if income >= 85528:
+elif income >= 85528:
     tax = 14839.02 + income - 85528 * 0.32
+
+if tax < 0:
+    tax = 0
 
 tax = round(tax, 0)
 print("Taxa fica:", tax, "Reais")
