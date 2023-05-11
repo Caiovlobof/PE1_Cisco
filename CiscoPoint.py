ab6e94ec8a0587e09bc5b6158947725b39b62387
Caiovlobof <caiovlobof@gmail.com>
2023-05-11 16:15:38 -0300

    checkpoint

--- CiscoPoint.py
+++ CiscoPoint.py
@@ -1,2 +1,7 @@
-n = int(input("Enter number:"))
-print(n >= 100)
+n1 = int(input("Type number one:"))
+n2 = int(input("Type number two:"))
+n3 = int(input("Type number three:"))
+largest_n = n1
+if n2 > largest_n: largest_n = n2
+if n3 > largest_n: largest_n = n3
+print("Largest number is:",largest_n)
