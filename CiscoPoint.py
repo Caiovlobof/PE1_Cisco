53741e953fd11ca403a0974835368b42390c9775
Caiovlobof <caiovlobof@gmail.com>
2023-05-11 17:28:45 -0300

    checkpoint

--- CiscoPoint.py
+++ CiscoPoint.py
@@ -1,8 +1,9 @@
-entrada = input("tell me something..")
+income = float(input("Digite aqui seu imposto: "))
 
-if entrada == "Spathiphyllum":
-    print("Spathiphyllum é a melhor fábrica de todos os tempos!")
-elif entrada == "spathiphyllum":
-    print("Não, eu quero um grande Spathiphyllum!")
-else:
-    print("Spathiphyllum! Not", entrada, "!")
+if income < 85528:
+    tax = income * 0.18 - 556.02
+if income >= 85528:
+    tax = 14839.02 + income - 85528 * 0.32
+
+tax = round(tax, 0)
+print("Taxa fica:", tax, "Reais")
